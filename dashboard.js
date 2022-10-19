da6f5d538f21af74dc673e189939da9a5a32a1af
edirciohub <84399911+edirciohub@users.noreply.github.com>
2022-10-19 23:47:06 +0200

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,80 @@
+/* globals Chart:false, feather:false */
+
+(() => {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+
+  const chatr = document.getElementById('myChart2').getContext('2d');
+  const myChart2 = new Chart(chatr, {
+    type: 'doughnut',
+        data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 506, 86, 1)',
+                'rgba(55, 506, 3, 1)',
+                'rgba(100, 100, 3, 1)',
+                
+            ],
+            
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+  });
+
+})()
